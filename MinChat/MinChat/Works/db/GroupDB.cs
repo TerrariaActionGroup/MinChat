47cccdcf4b0594cf6270f78beff0ac6e84a6d8a2
ID3055 <ID3055@qq.com>
2015-12-28 22:43:36 +0800

    budiao

--- MinChat/MinChat/Works/db/GroupDB.cs
+++ MinChat/MinChat/Works/db/GroupDB.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SQLite;
 using MinChat.Communications.bean;
+using System.Data.SQLite;
 
 namespace MinChat.Works.db
 {
--- MinChat/MinChat/Works/db/SessionDB.cs
+++ MinChat/MinChat/Works/db/SessionDB.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.SQLite;
 using MinChat.Communications;
-using MinChat.Works.db;
+using System.Data.SQLite;
 
 namespace MinChat.Works.db
 {
